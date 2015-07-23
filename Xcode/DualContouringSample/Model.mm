48ef109658164806393e1f60b1bb88b7ed4ed2d8
Brent Gulanowski <bgulanowski@gmail.com>
2015-07-23 09:14:47 -0400

    Attempt to use serializable version of vertices but did not work due to C++ being too hard.

--- Xcode/DualContouringSample/Model.mm
+++ Xcode/DualContouringSample/Model.mm
@@ -15,8 +15,14 @@ extern "C" {
 #import "mesh.h"
 #import "octree.h"
 
+#import "serialization.h"
+#import "SerializableVertex.hpp"
+
 #import <OpenGL/gl3.h>
 #import <AppKit/AppKit.h>
+
+#import <fstream>
+#import <string>
 
 const static NSUInteger MAX_THRESHOLDS = 5;
 const static CGFloat THRESHOLDS[MAX_THRESHOLDS] = { -1.f, 0.1f, 1.f, 10.f, 50.f };
@@ -47,8 +53,49 @@ const static int octreeSize = 64;
 
 - (void)reloadInContext:(NSOpenGLContext *)context {
 
+
     _thresholdIndex = (_thresholdIndex + 1) % MAX_THRESHOLDS;
+
+#if 0
+    // Trying to subclass to mix in serializable but not working!
+    __block std::vector<SerializableVertex> vertices;
+    __block IndexBuffer indices;
     
+    std::string vertexFileName("dual_contouring_vertices.dat");
+    std::string indexFileName("dual_countouring_indices.dat");
+    
+    std::ifstream vertexFile(vertexFileName, std::ios::in | std::ios::binary);
+    
+    std::ifstream indexFile(indexFileName, std::ios::in | std::ios::binary);
+    
+    if (vertexFile.good() && indexFile.good()) {
+        serialize::read(vertexFile, vertices);
+        vertexFile.close();
+        
+        serialize::read(indexFile, indices);
+        indexFile.close();
+    }
+    
+    if (vertices.size() == 0 || indices.size() == 0) {
+        
+        LogBlockDuration(@"Generate Model", ^{
+            _root = BuildOctree(glm::ivec3(-octreeSize / 2), octreeSize, THRESHOLDS[_thresholdIndex]);
+            GenerateMeshFromOctree(_root, vertices, indices);
+            GenerateMeshFromOctree(_root, (VertexBuffer&)vertices, indices);
+        });
+        
+        std::ofstream file(vertexFileName, std::ios::out | std::ios::binary | std::ios::trunc);
+        serialize::write(file, vertices);
+        file.flush();
+        file.close();
+        
+        file = std::ofstream(indexFileName, std::ios::out | std::ios::binary | std::ios::trunc);
+        serialize::write(file, indices);
+        file.flush();
+        file.close();
+    }
+
+#else
     __block VertexBuffer vertices;
     __block IndexBuffer indices;
     
@@ -57,6 +104,7 @@ const static int octreeSize = 64;
         GenerateMeshFromOctree(_root, vertices, indices);
         GenerateMeshFromOctree(_root, (VertexBuffer&)vertices, indices);
     });
+#endif
     
     [context lock];
     [context makeCurrentContext];
