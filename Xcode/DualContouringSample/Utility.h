c90fd4a5d076955dde353eb3fcdb823a676cc934
Brent Gulanowski <bgulanowski@gmail.com>
2015-07-23 07:27:31 -0400

    Add a simple utility function to time operations.

---
+++ Xcode/DualContouringSample/Utility.h
@@ -0,0 +1,13 @@
+//
+//  Utility.h
+//  DualContouringSample
+//
+//  Created by Brent Gulanowski on 2015-07-17.
+//  Copyright © 2015 Lichen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
+@end
---
+++ Xcode/DualContouringSample/Utility.m
@@ -0,0 +1,13 @@
+//
+//  Utility.m
+//  DualContouringSample
+//
+//  Created by Brent Gulanowski on 2015-07-17.
+//  Copyright © 2015 Lichen Labs. All rights reserved.
+//
+
+#import "Utility.h"
+
+@implementation Utility
+
+@end
